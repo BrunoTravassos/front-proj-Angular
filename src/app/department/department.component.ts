8b46976a103fe623195db78b6e60ad9622bb1e35
BrunoTravassos <brunodefrontin@gmail.com>
2021-10-14 15:15:15 -0300

    commit snckbar

--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBarContainer } from '@angular/material/snack-bar';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { Department } from '../department';
@@ -17,12 +18,28 @@ export class DepartmentComponent implements OnInit {
 
   private unsubscribe$: Subject<any> = new Subject();
 
-  constructor(private departmentService: DepartmentService) { }
+  constructor(
+    private departmentService: DepartmentService,
+    private snackbar: MatSnackBarContainer
+  ) { }
 
   ngOnInit() {
     this.departmentService.get()
       .pipe(takeUntil(this.unsubscribe$))
     .subscribe((deps)=>this.departments=deps)
+  }
+
+  clearFields() {
+    this.depName = '';
+    this.depEdit = null;
+  }
+
+  cancel() {
+    this.clearFields();
+  }
+
+  notify(msg: string) {
+    this.snackbar.open
   }
 
 }
